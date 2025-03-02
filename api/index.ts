b3c457c40feaf1c3b98d715e5c16fffe9497e236
hbadr13 <hamza1937badr@gmail.com>
2025-03-02 14:47:59 +0000

    ss4

---
+++ api/index.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response, Router } from 'express'
+import { Express } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+// import playerRouter from '@routes/player/player.router';
+
+dotenv.config()
+
+console.log(process.env.REACT_APP_CLOUD_NAME)
+console.log(process.env.REACT_APP_API_KEY)
+console.log(process.env.REACT_APP_API_SECRET)
+
+const port: string = process.env.PORT || '4000'
+const app: Express = express()
+
+app.listen((port), () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+})
+app.use(cors(
+    {
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        origin: ['http://localhost:3000', 'http://localhost:5173'],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma",
+        ],
+        credentials: true,
+    }
+))
+app.use(cookieParser());
+app.use(express.json())
+const r = Router()
+r.get('/', async (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Hello SofascoreCloning' })
+})
+app.use('/', r)
+// app.use('/api/v1/player', playerRouter)
